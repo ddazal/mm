e087dd722bde68745351a8c440b8e254b54475d7
David Daza <03dazal@gmail.com>
2021-01-16 21:32:42 -0500

    update event data when info is changed

--- ui/src/app/schedule-wizard/components/calendar/calendar.component.ts
+++ ui/src/app/schedule-wizard/components/calendar/calendar.component.ts
@@ -1,7 +1,8 @@
 import * as moment from 'moment';
 import esLocale from '@fullcalendar/core/locales/es';
-import { CalendarOptions } from '@fullcalendar/angular';
-import { Component, Input, OnInit } from '@angular/core';
+import { CalendarOptions, FullCalendarComponent } from '@fullcalendar/angular';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
 import { StepComponent } from '../../step.component';
 import { WizardData } from '../../wizard-data';
 
@@ -29,8 +30,10 @@ export class CalendarComponent implements OnInit, StepComponent {
       hour12: false,
     },
     events: this.events,
+    eventClassNames: ['mm-event'],
     editable: true,
-    eventClick: this.updateEvent.bind(this),
+    eventResize: this.updateEvent.bind(this),
+    eventDrop: this.updateEvent.bind(this),
     dateClick: this.createEvent.bind(this),
   };
 
@@ -45,6 +48,7 @@ export class CalendarComponent implements OnInit, StepComponent {
     const start = moment(info.dateStr);
     const end = moment(start).add(1, 'hours');
     this.events = this.events.concat({
+      id: uuidv4(),
       title: this.data.eventTitle,
       start: start.format(),
       end: end.format(),
@@ -52,8 +56,11 @@ export class CalendarComponent implements OnInit, StepComponent {
     this.calendarOptions.events = this.events;
   }
 
-  updateEvent(info): void {
-    console.log(info);
+  updateEvent({ event }): void {
+    const eventIndex = this.events.findIndex((e) => e.id === event.id);
+    const { id, title, start, end } = event;
+    this.events.splice(eventIndex, 1, { event: { id, title, start, end } });
+    this.calendarOptions.events = this.events;
   }
 
   submit(): { isValid; data } {
