353e2e414a52bfda0ba93d3512a9ab8d49ec51c9
David Daza <03dazal@gmail.com>
2021-03-13 15:49:38 -0500

    add tooltip to event box in calendar

--- ui/src/app/meeting-wizard/components/meeting-options/meeting-options.component.ts
+++ ui/src/app/meeting-wizard/components/meeting-options/meeting-options.component.ts
@@ -1,8 +1,9 @@
 import * as moment from 'moment';
 import esLocale from '@fullcalendar/core/locales/es';
-import { CalendarOptions } from '@fullcalendar/angular';
-import { Component, Input, OnInit } from '@angular/core';
+import { Calendar, CalendarOptions, FullCalendarComponent } from '@fullcalendar/angular';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { v4 as uuidv4 } from 'uuid';
+import tippy from 'tippy.js';
 
 import { StepComponent } from '../../models/step-component.model';
 import { MeetingData } from '../../models/meeting-data.model';
@@ -11,11 +12,12 @@ import { WizardService } from '../../../services/wizard.service';
 
 @Component({
   selector: 'app-meeting-options',
-  templateUrl: './meeting-options.component.html',
+  templateUrl: './meeting-options.component.html'
 })
 export class MeetingOptionsComponent implements OnInit, StepComponent {
   @Input() data: MeetingData;
   @Input() eventTitle: string;
+  @ViewChild('calendar') calendarComponent: FullCalendarComponent;
 
   events: MeetingOption[] = [];
 
@@ -36,6 +38,8 @@ export class MeetingOptionsComponent implements OnInit, StepComponent {
     events: this.events,
     eventColor: '#0331f4',
     editable: true,
+    eventClick: this.clickedEvent.bind(this),
+    eventDidMount: this.eventAdded.bind(this),
     eventResize: this.updateEvent.bind(this),
     eventDrop: this.updateEvent.bind(this),
     dateClick: this.createEvent.bind(this),
@@ -48,32 +52,73 @@ export class MeetingOptionsComponent implements OnInit, StepComponent {
     this.calendarOptions.events = this.events;
   }
 
+  eventAdded({ event, el }): void {
+    const e = this.calendar.getEventById(event.id);
+    const tippyInstace = tippy(el, {
+      allowHTML: true,
+      interactive: true,
+      trigger: 'click',
+      theme: 'light',
+      appendTo: document.body
+    });
+    e.setExtendedProp('tippy', tippyInstace);
+  }
+
+  clickedEvent({ event }): void {
+    const template = document.getElementById(event.id);
+    event.extendedProps.tippy.setContent(template.innerHTML);
+  }
+
   createEvent({ dateStr }): void {
     const start = moment(dateStr);
     const end = moment(start).add(1, 'hours');
-    this.events = this.events.concat({
+    const event = {
       id: uuidv4(),
       title: this.data.title,
       start: start.format(),
       end: end.format(),
-    });
-    this.updateCalendarEvents();
+    };
+    this.events = this.events.concat(event);
+    this.calendar.addEvent(event);
+    this.updateWizardData();
+  }
+
+  deleteEvent(id: string): void {
+    const deletedEvent = this.calendar.getEventById(id);
+    const eventIndex = this.findEventIndex(id);
+    deletedEvent.remove();
+    this.events.splice(eventIndex, 1);
+    this.updateWizardData();
   }
 
   updateEvent({ event }): void {
-    const eventIndex = this.events.findIndex((e) => e.id === event.id);
-    const { id, title, start, end } = event;
-    this.events.splice(eventIndex, 1, { id, title, start, end });
-    this.updateCalendarEvents();
+    const updatedEvent = this.calendar.getEventById(event.id);
+    const eventIndex = this.findEventIndex(event.id);
+
+    updatedEvent.setDates(updatedEvent.start, updatedEvent.end);
+    this.events.splice(eventIndex, 1, {
+      id: event.id,
+      title: event.title,
+      start: event.start,
+      end: event.end
+    });
+    this.updateWizardData();
   }
 
-  updateCalendarEvents(): void {
+  updateWizardData(): void {
     this.ws.updateData({ ...this.data, options: this.events });
-    this.calendarOptions.events = this.events;
   }
 
   submit(): { isValid; data; error } {
     const isValid = !!this.events.length;
     return { isValid, data: { options: this.events }, error: 'Crea, al menos, una opción' };
+  }
+
+  findEventIndex(id: string): number {
+    return this.events.findIndex((e) => e.id === id);
+  }
+
+  get calendar(): Calendar {
+    return this.calendarComponent.getApi();
   }
 }
